214a3d5bdf06c3bc65de827f4eb5b259e1a5924f
jyooooo <0vf60m373241b3j@ezweb.ne.jp>
2020-04-11 10:18:12 +0900

    初期のコミット

---
+++ src/main/java/RaiseTech/DateCalculation/DateCalculationApplication.java
@@ -0,0 +1,13 @@
+package RaiseTech.DateCalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateCalculationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/RaiseTech/DateCalculation/ServletInitializer.java
@@ -0,0 +1,13 @@
+package RaiseTech.DateCalculation;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DateCalculationApplication.class);
+	}
+
+}
