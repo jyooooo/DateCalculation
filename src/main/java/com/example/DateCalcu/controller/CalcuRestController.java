f9b0d27f37454fefa0911f0501059d7b85c8fbac
jyooooo <0vf60m373241b3j@ezweb.ne.jp>
2020-05-21 11:15:49 +0900

    RestController一部修正

--- src/main/java/com/example/DateCalcu/controller/CalcuRestController.java
+++ src/main/java/com/example/DateCalcu/controller/CalcuRestController.java
@@ -5,25 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.DateCalcu.domain.DomainForm;
 import com.example.DateCalcu.service.CalcuService;
 
 @RestController
+@RequestMapping("/api")
 public class CalcuRestController {
 
 	@Autowired
 	public CalcuService calcuService;
 
-	@GetMapping("/api")
-	//計算式一覧取得
+	@GetMapping
+	//全ての計算式を取得
 	public List<DomainForm> findAll(){
 
 	return calcuService.findAll();
 	}
-	@GetMapping("/api/{dateId:.+}")
-	//計算式一覧取得
+	@GetMapping("/{dateId}")
+	//指定した日付IDの計算式を取得
 	public DomainForm findONE(@PathVariable("dateId")String dateId){
 
 	return calcuService.findOne(dateId);
