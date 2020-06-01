43b824012353815c7fcbd1934f72eb77fc69ccb5
jyooooo <0vf60m373241b3j@ezweb.ne.jp>
2020-06-01 23:29:48 +0900

    CreateControllerテストコード実装中

--- src/test/java/com/example/DateCalcu/controller/CreateControllerTest.java
+++ src/test/java/com/example/DateCalcu/controller/CreateControllerTest.java
@@ -41,14 +41,125 @@ public class CreateControllerTest {
 	}
 
 	@Test
-	public void バリデーションエラーが発生した場合() throws Exception {
+	public void 新規登録画面の日付IDが空白のまま登録処理に移った場合例外情報が入った状態で画面が返る事() throws Exception {
 		DomainForm form = new DomainForm();
-		form.setDateId("hoge");
+		//空白をセット
+		form.setDateId(" ");
+		form.setDateName("テスト");
+		mockMvc.perform((post("/create")).flashAttr("form", form))
+				//エラー判定
+				.andExpect(model().hasErrors())
+				//指定のviewを返すか？
+				.andExpect(view().name("register"));
+	}
+
+	@Test
+	public void 新規登録画面の日付IDが空のまま登録処理に移った場合例外情報が入った状態で画面が返る事() throws Exception {
+		DomainForm form = new DomainForm();
+		//空をセット
+		form.setDateName("");
+		form.setDateId("テスト");
 
 		mockMvc.perform((post("/create")).flashAttr("form", form))
 				//エラー判定
 				.andExpect(model().hasErrors())
 				//指定のviewを返すか？
+				.andExpect(view().name("register"));
+	}
+
+	@Test
+	public void 新規登録画面の日付IDがNullのまま登録処理に移った場合例外情報が入った状態で画面が返る事() throws Exception {
+		DomainForm form = new DomainForm();
+		//日付名のみセット
+		form.setDateName("テスト");
+
+		mockMvc.perform((post("/create")).flashAttr("form", form))
+				//エラー判定
+				.andExpect(model().hasErrors())
+				//指定のviewを返すか？
+				.andExpect(view().name("register"));
+	}
+
+	@Test
+	public void 新規登録画面の日付IDが6桁以上のまま登録処理に移った場合例外情報が入った状態で画面が返る事() throws Exception {
+		DomainForm form = new DomainForm();
+		//6桁の日付IDをセット
+		form.setDateId("1234567");
+		form.setDateName("テスト");
+
+		mockMvc.perform((post("/create")).flashAttr("form", form))
+				//エラー判定
+				.andExpect(model().hasErrors())
+				//指定のviewを返すか？
+				.andExpect(view().name("register"));
+	}
+	@Test
+	public void 新規登録画面の日付名が空白のまま登録処理に移った場合例外情報が入った状態で画面が返る事() throws Exception {
+		DomainForm form = new DomainForm();
+		//空白をセット
+		form.setDateName(" ");
+		form.setDateId("TEST");
+		mockMvc.perform((post("/create")).flashAttr("form", form))
+				//エラー判定
+				.andExpect(model().hasErrors())
+				//指定のviewを返すか？
+				.andExpect(view().name("register"));
+	}
+
+	@Test
+	public void 新規登録画面の日付名が空のまま登録処理に移った場合例外情報が入った状態で画面が返る事() throws Exception {
+		DomainForm form = new DomainForm();
+		//空をセット
+		form.setDateName("");
+		form.setDateId("TEST");
+
+		mockMvc.perform((post("/create")).flashAttr("form", form))
+				//エラー判定
+				.andExpect(model().hasErrors())
+				//指定のviewを返すか？
+				.andExpect(view().name("register"));
+	}
+
+	@Test
+	public void 新規登録画面の日付名がNullのまま登録処理に移った場合例外情報が入った状態で画面が返る事() throws Exception {
+		DomainForm form = new DomainForm();
+		//日付IDのみセット
+		form.setDateId("テスト");
+
+		mockMvc.perform((post("/create")).flashAttr("form", form))
+				//エラー判定
+				.andExpect(model().hasErrors())
+				//指定のviewを返すか？
+				.andExpect(view().name("register"));
+	}
+
+	@Test
+	public void 新規登録画面の日付名が32桁以上のまま登録処理に移った場合例外情報が入った状態で画面が返る事() throws Exception {
+		DomainForm form = new DomainForm();
+		//6桁の日付IDをセット
+		form.setDateName("0123456789ABCDEF101112131415161718");
+		form.setDateId("テスト");
+
+		mockMvc.perform((post("/create")).flashAttr("form", form))
+				//エラー判定
+				.andExpect(model().hasErrors())
+				//指定のviewを返すか？
+				.andExpect(view().name("register"));
+	}
+	@Test
+	public void バリデーションエラーが発生しない場合() throws Exception {
+		DomainForm form = new DomainForm();
+
+		form.setDateId("hoge");
+
+		mockMvc.perform((post("/create")).flashAttr("form", form))
+				//エラー判定
+				.andExpect(model().hasNoErrors())
+				.andExpect(model().attribute("form", form))
+				//指定のviewを返すか？
 				.andExpect(view().name("index"));
+		//CalcuService.saveがformというインスタンスを引数に1回呼ばれることをテスト
+		verify(mockFormService, times(1)).save(form);
+
 	}
 }
